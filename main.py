1f41f77689e24ac4d7516143845b849e497f5b15
elf <elorian@yandex.ru>
2022-02-08 18:32:26 +0300

    create longpooling request in cycle

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 import requests
 from environs import Env
 
@@ -6,19 +8,30 @@ def get_work_status(url, token):
     headers = {
         'Authorization': f'Token {token}'
     }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    return response.json()
+    params = {
+        'timestamp': 1644330692.0869882
+    }
+    while True:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        logging.info(response.json())
 
 
 def main():
     env = Env()
     env.read_env()
 
-    work_status_link = 'https://dvmn.org/api/user_reviews/'
+    work_status_link = 'https://dvmn.org/api/long_polling/'
     devman_token = env.str('TOKEN')
 
-    print(get_work_status(work_status_link, devman_token))
+    get_work_status(work_status_link, devman_token)
 
 
-main()
+if __name__ == "__main__":
+    logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
+            filename='logs.lod',
+            filemode='w',
+        )
+    main()
